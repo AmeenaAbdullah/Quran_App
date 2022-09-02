8c50ced2851abcdc73717567dd95ce0fed23fea6
Amena Abdullah <you@example.com>
2022-09-03 01:46:54 +0500

    updated

---
+++ app/src/main/java/com/example/myquranapp/FrontPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.myquranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrontPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+    }
+}
---
+++ app/src/main/java/com/example/myquranapp/SurahActivity.java
@@ -0,0 +1,61 @@
+package com.example.myquranapp;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class SurahActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.surah);
+
+        TextView name=findViewById(R.id.surahName);
+        ListView AllSurahList = findViewById(R.id.surahList);
+        DBhelper dbHelper  = new DBhelper(SurahActivity.this);
+       Intent i =getIntent();
+        int index = Integer.parseInt(i.getStringExtra("index"));
+        ArrayList<tayah> list =dbHelper.Surah(index+1);
+       SurahAdapter sura=new SurahAdapter(this,list);
+        String namesurah = i.getStringExtra("surahName");
+        AllSurahList.setAdapter(sura);
+        name.setText(namesurah);
+//        TextView fullsurah=findViewById(R.id.fullsurah);
+//        DBhelper dbHelper  = new DBhelper(SurahActivity.this);
+//        Intent i =getIntent();
+//        int index = Integer.parseInt(i.getStringExtra("index"));
+//
+//
+//        String namesurah = i.getStringExtra("surahName");
+//        ArrayList<tayah> list =dbHelper.Surah(index+1);
+//
+//        String content = new String();
+//        content="";
+//        for (int j = 0; j < list.size(); j++) {
+//            tayah t=list.get(j);
+//            content+= t.getArabicText().toString();
+//            // will print each index as it loops
+//        }
+//
+//        fullsurah.setText(content);
+//        name.setText(namesurah);
+
+//    Typeface typeface = Typeface.createFromAsset( getAssets(),  "noorehuda.ttf");
+//        content.setTypeface(typeface);
+
+ }
+}
