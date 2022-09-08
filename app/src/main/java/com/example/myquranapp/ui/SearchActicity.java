541c3041f7aa317f47be57d63628a37af181eee1
Amena Abdullah <you@example.com>
2022-09-09 01:10:02 +0500

    search

--- app/src/main/java/com/example/myquranapp/ui/SearchActicity.java
+++ app/src/main/java/com/example/myquranapp/ui/SearchActicity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myquranapp.R;
+
 public class SearchActicity extends AppCompatActivity {
 
     @Override
