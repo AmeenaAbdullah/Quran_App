d1962eb0723f31c38fa5cba5499ce0beba47ac5d
Amena Abdullah <you@example.com>
2022-09-10 01:43:01 +0500

    done searching

--- app/src/main/java/com/example/myquranapp/SearchActivity.java
+++ app/src/main/java/com/example/myquranapp/SearchActivity.java
@@ -15,12 +15,13 @@ import android.widget.SearchView;
 
 import java.util.ArrayList;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     RecyclerView recyclerView;
     All_Surah_nameAdapter adapter;
     RecyclerView.LayoutManager layoutManager;
     ArrayList<tsurah> list;
-
+    DBhelper dbHelper;
+    int T_index;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,29 +30,33 @@ public class SearchActivity extends AppCompatActivity {
 //        EditText text=findViewById(R.id.editsurah);
         SearchView simpleSearchView=(SearchView)findViewById(R.id.simpleSearchView);
         Intent i = getIntent();
-        int T_index = Integer.parseInt(i.getStringExtra("value"));
+         T_index = Integer.parseInt(i.getStringExtra("value"));
 
-        DBhelper dbHelper = new DBhelper(SearchActivity.this);
+      dbHelper = new DBhelper(SearchActivity.this);
 
 
         simpleSearchView.setOnQueryTextListener((SearchView.OnQueryTextListener) this);
-//        search.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String searchtext=text.getText().toString();
-//                list = dbHelper.searchdata(searchtext);    //Get Data from DB
-//                recyclerView = findViewById(R.id.recyclerViewsearch);
-//
-//                recyclerView.setHasFixedSize(true);
-//                layoutManager = new LinearLayoutManager(SearchActivity.this,
-//                        LinearLayoutManager.VERTICAL,
-//                        false);
-//                recyclerView.setLayoutManager(layoutManager);
-//                adapter = new All_Surah_nameAdapter(getApplicationContext(), list, T_index);
-//
-//                recyclerView.setAdapter(adapter);
-//            }
-//        });
 
+    }
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+               list = dbHelper.searchdata(newText);    //Get Data from DB
+                recyclerView = findViewById(R.id.recyclerViewsearch);
+
+                recyclerView.setHasFixedSize(true);
+                layoutManager = new LinearLayoutManager(SearchActivity.this,
+                        LinearLayoutManager.VERTICAL,
+                        false);
+                recyclerView.setLayoutManager(layoutManager);
+                adapter = new All_Surah_nameAdapter(getApplicationContext(), list, T_index);
+
+                recyclerView.setAdapter(adapter);
+                return false;
     }
 }
